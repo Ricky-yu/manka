4e81e88834abd825898bf7277712494f8e4aec09
Ricky-yu <ts00484515@gmail.com>
2020-08-11 23:14:09 +0900

    rm grb

---
+++ Manka/Manka/Pods/Target Support Files/Pods-MankaTests/Pods-MankaTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MankaTests : NSObject
+@end
+@implementation PodsDummy_Pods_MankaTests
+@end
---
+++ Manka/Manka/Pods/Target Support Files/Pods-MankaUITests/Pods-MankaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MankaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MankaUITests
+@end

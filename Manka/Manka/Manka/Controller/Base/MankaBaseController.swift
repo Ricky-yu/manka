d6c840f10ba1d221710cea528c23267d2886a53b
Ricky-yu <ts00484515@gmail.com>
2020-08-12 00:58:07 +0900

    add Base Navigation TabBar Controller for all ViewController

---
+++ Manka/Manka/Manka/Controller/Base/MankaBaseController.swift
@@ -0,0 +1,42 @@
+//
+//  MankaBaseController.swift
+//  Manka
+//
+//  Created by CHEN SINYU on 2020/08/11.
+//  Copyright © 2020 CHEN SINYU. All rights reserved.
+//
+
+import UIKit
+
+class MankaBaseController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.background
+        
+        if #available(iOS 11.0, *) {
+            UIScrollView.appearance().contentInsetAdjustmentBehavior = .never
+        } else {
+            automaticallyAdjustsScrollViewInsets = false
+        }
+        setupLayout()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configNavigationBar()
+    }
+    
+    func setupLayout() {}
+    
+    func configNavigationBar() {
+    
+    }
+}
+
+extension MankaBaseController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
---
+++ Manka/Manka/Manka/Controller/Base/MankaTabBarController.swift
@@ -0,0 +1,46 @@
+//
+//  MankaTabBarController.swift
+//  Manka
+//
+//  Created by CHEN SINYU on 2020/08/11.
+//  Copyright © 2020 CHEN SINYU. All rights reserved.
+//
+
+import UIKit
+
+class MankaTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.isTranslucent = false
+        setupLayout()
+    }
+    
+    func setupLayout(){
+        let classVC = ManKaCateController()
+        addChildController(classVC,
+                               title: "カテゴリー",
+                               image: UIImage(named: "tab_class"),
+                               selectedImage: UIImage(named: "tab_class_S"))
+    }
+    
+    func addChildController(_ childController: UIViewController, title:String?, image:UIImage? ,selectedImage:UIImage?) {
+        
+        childController.title = title
+        childController.tabBarItem = UITabBarItem(title: nil,
+                                                  image: image?.withRenderingMode(.alwaysOriginal),
+                                                  selectedImage: selectedImage?.withRenderingMode(.alwaysOriginal))
+        
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            childController.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        }
+        addChild(MankaNaviController(rootViewController: childController))
+    }
+}
+
+extension MankaTabBarController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        guard let select = selectedViewController else { return .lightContent }
+        return select.preferredStatusBarStyle
+    }
+}

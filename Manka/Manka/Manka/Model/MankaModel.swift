034f49d6bc4baa6fe8b4fc739f7e9a5c91bd4f45
Ricky-yu <ts00484515@gmail.com>
2020-08-16 17:34:41 +0900

    add Model for Comment Detail Chapter

--- Manka/Manka/Manka/Model/MankaModel.swift
+++ Manka/Manka/Manka/Model/MankaModel.swift
@@ -149,3 +149,174 @@ struct MankaExtModel: HandyJSON {
     var key: String?
     var val: String?
 }
+
+struct MankaOtherWorkModel: HandyJSON {
+    var comicId: Int = 0
+    var coverUrl: String?
+    var name: String?
+    var passChapterNum: Int = 0
+}
+
+struct MankaDetailStaticModel: HandyJSON {
+    var comic: MankaComicStaticModel?
+    var chapter_list: [MankaChapterStaticModel]?
+    var otherWorks: [MankaOtherWorkModel]?
+}
+
+struct MankaDetailRealtimeModel: HandyJSON {
+    var comic: MankaComicRealtimeModel?
+    var chapter_list: [MankaChapterRealtimeModel]?
+}
+
+struct MankaComicRealtimeModel: HandyJSON {
+    var comic_id: Int = 0
+    var user_id: Int = 0
+    var status: Int = 0
+    var click_total: String?
+    var total_ticket: String?
+    var comment_total: String?
+    var total_tucao: String?
+    var favorite_total: String?
+    var gift_total: String?
+    var monthly_ticket: String?
+    var vip_discount: Double = 0
+    var is_vip_free: Bool = false
+    var is_free: Bool = false
+    var is_vip_buy: Bool = false
+    var is_auto_buy: Bool = false
+}
+
+struct MankaChapterRealtimeModel: HandyJSON {
+    var vip_images: Int = 0
+    var is_view: Bool = false
+    var chapter_id: Int = 0
+    var buyed: Bool = false
+    var buy_price: String?
+    var read_state: Int = 0
+    var is_free: Bool = false
+}
+
+struct MankaAuthorModel: HandyJSON {
+    var id: Int = 0
+    var avatar: String?
+    var name: String?
+}
+
+struct MankaClassifyTagModel: HandyJSON {
+    var name: String?
+    var argName: String?
+    var argVal: Int = 0
+}
+
+struct MankaComicStaticModel: HandyJSON {
+    var name: String?
+    var comic_id: Int = 0
+    var short_description: String?
+    var accredit: Int = 0
+    var cover: String?
+    var is_vip: Int = 0
+    var type: Int = 0
+    var ori: String?
+    var theme_ids: [String]?
+    var series_status: Int = 0
+    var last_update_time: TimeInterval = 0
+    var description: String?
+    var cate_id: String?
+    var status: Int = 0
+    var thread_id: Int = 0
+    var last_update_week: String?
+    var wideCover: String?
+    var classifyTags: [MankaClassifyTagModel]?
+    var is_week: Bool = false
+    var comic_color: String?
+    var author: MankaAuthorModel?
+    var is_dub: Bool = false
+}
+
+struct MankaUImHightModel: HandyJSON {
+    var height: Int = 0
+    var width: Int = 0
+}
+
+struct MankaChapterStaticModel: HandyJSON {
+    var chapter_id: Int = 0
+    var name: String?
+    var image_total: Int = 0
+    var type: Int = 0
+    var price:String?
+    var size: Int32 = 0
+    var pass_time: TimeInterval = 0
+    var release_time: TimeInterval = 0
+    var zip_high_webp: Int = 0
+    var is_new: Bool = false
+    var has_locked_image: Bool = false
+    var imHightArr: [[MankaUImHightModel]]?
+    var countImHightArr: Int = 0
+}
+struct MankaGuessLikeModel: HandyJSON {
+    var normal: Bool = false
+    var last_modified: Int = 0
+    var comics: [MankaComicModel]?
+}
+
+struct MankaLevelModel: HandyJSON {
+    var album_size: CGFloat = 0
+    var exp_speed: Float = 0
+    var favorite_num: Int = 0
+    var level: Int = 0
+    var max: Int = 0
+    var min_exp: Int = 0
+    var ticket: Int = 0
+    var wage: Int = 0
+}
+
+struct MankaCommentModel: HandyJSON {
+    var cate: Int = 0
+    var color: String?
+    var comic_author: Int = 0
+    var comment_from: String?
+    var comment_id: Int = 0
+    var content: String?
+    var content_filter: String?
+    var create_time: TimeInterval = 0
+    var create_time_str: String?
+    var exp: Float = 0
+    var face: String?
+    var face_type: Int = 0
+    var floor: Int = 0
+    var gift_img: String?
+    var gift_num: Int = 0
+    var group_admin: Bool = false
+    var group_author: Bool = false
+    var group_custom: Bool = false
+    var group_user: Bool = false
+    var id: Int = 0
+    var imageList: [Any]?
+    var ip: String?
+    var is_choice: Bool = false
+    var is_delete: Bool = false
+    var is_lock: Bool = false
+    var is_up: Bool = false
+    var level: MankaLevelModel?
+    var likeCount: Int = 0
+    var likeState: Int = 0
+    var nickname: String?
+    var online_time: TimeInterval = 0
+    var sex: String?
+    var ticketNum: Int = 0
+    var title: String?
+    var total_reply: Int = 0
+    var user_id: Int = 0
+    var vip_exp: Int = 0
+}
+
+struct  MankaCommentListModel: HandyJSON {
+    var commentCount: Int = 0
+    var commentList: [MankaCommentModel]?
+    var hasMore: Bool = false
+    var objectId: Int = 0
+    var objectType: String?
+    var page: Int = 0
+    var pageCount: Int = 0
+    var serverNextPage: Int = 0
+}
